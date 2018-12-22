186c9e791be93131a2b805c9630b937f2d2ff2d4
wangyu <wangyu_eva@163.com>
2018-12-23 01:36:23 +0800

    init commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangyu
+ * 2018/12/23 01:30
+ */
+@RestController
+@RequestMapping("home")
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello Jenkins!";
+    }
+}
